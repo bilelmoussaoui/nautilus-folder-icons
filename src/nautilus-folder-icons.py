895f3fbb1a795962e33171710fe04214d4a4efaa
Bilal Elmoussaoui <bil.elmoussaoui@gmail.com>
2017-06-13 22:05:45 +0200

    clean meson build system

--- src/nautilus-folder-icons.py
+++ src/nautilus-folder-icons.py
@@ -4,18 +4,18 @@ Change your nautilus directories icons easily
 
 Author : Bilal Elmoussaoui (bil.elmoussaoui@gmail.com)
 Version : 1.0
-Website : https://github.com/bil-elmoussaoui/nautilus-git
+Website : https://github.com/bil-elmoussaoui/nautilus-folder-icons
 Licence : GPL-3.0
-nautilus-git is free software: you can redistribute it and/or
+nautilus-folder-icons is free software: you can redistribute it and/or
 modify it under the terms of the GNU General Public License as published
 by the Free Software Foundation, either version 3 of the License, or
 (at your option) any later version.
-nautilus-git is distributed in the hope that it will be useful,
+nautilus-folder-icons is distributed in the hope that it will be useful,
 but WITHOUT ANY WARRANTY; without even the implied warranty of
 MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 GNU General Public License for more details.
 You should have received a copy of the GNU General Public License
-along with nautilus-git. If not, see <http://www.gnu.org/licenses/>.
+along with nautilus-folder-icons. If not, see <http://www.gnu.org/licenses/>.
 """
 from gettext import textdomain, gettext as _
 from os import path
